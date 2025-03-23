8b3add458f6621bbaaf81175e9e7af1c37b161fb
dazy821 <77889723+dazy821@users.noreply.github.com>
2025-03-23 17:20:39 -0400

    added file to connect to db and replaced previous fucntions with functions that connect to db sessions

--- main.py
+++ main.py
@@ -9,22 +9,41 @@ tasks = [
     Task()
 ]
 
+@app.get("/")
+def root():
+    return "hello world"
+
 @app.get("/get-tasks") # get("create any address name you want") is a built in function, we're using on our variable "app". The address name is the end of your url and will describe what the get function is doing
 def get_all_tasks(): #creating a function."def" defines the fucntion followed by its name
     return tasks # get retrieves information. so we want to retrieve the list of tasks named "tasks"
 
-# @app.get("/") # get("create any address name you want") is a  built in function, we're using on our variable "app"
-# def get_task(): #creating a function."def" defines the fucntion followed by its name
-#     return "Hello"
+@app.get("/get-a-task/{task_id}") # get("create any address name you want") is a  built in function, we're using on our variable "app"
+def get_a_task(task_id: int): #creating a function."def" defines the fucntion followed by its name
+    for task in tasks:
+        if task_id == task_id:
+            return task
+    return "Task not found"
+        
 
-# @app.put("/")
-# def update_task(): 
-#     return "Hello"
+@app.put("/update-task/{task_id}")
+def update_task(task_id: int, updated_task: Task): 
+    for task in tasks:
+        if tasks.id == task_id:
+            task.description = updated_task.description
+            task.isComplete = updated_task.isComplete
+            return "Updated task"
+    return "Task not updated"
+    
 
-# @app.post("/") 
-# def create_task(): 
-#     return "Hello"
+@app.post("/create-task") 
+def create_task(task: Task): 
+    tasks.append(task)
+    return "Task Added"
 
-# @app.delete("/") 
-# def delete_task(): 
-#     return "Hello"
+@app.delete("/delete-task/{task_id}") 
+def delete_task(task_id:int): 
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(index)
+            return "Deleted task"
+    return "Task not deleted"
